a4b406a135f103b5ff4c2d650ed2d106126af19b
yhl50333 <573087829@qq.com>
2020-06-06 16:15:05 +0800

    dcc
    a

--- src/main/java/com/example/controller/log/LogController.java
+++ src/main/java/com/example/controller/log/LogController.java
@@ -2,7 +2,7 @@ package com.example.controller.log;
 
 import com.example.dao.UserService;
 import com.example.entity.JsonResult;
-import org.apache.ibatis.annotations.Param;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
